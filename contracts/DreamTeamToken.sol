26653f449ff1e611d7083517ccc2b269abecce91
dreamteamgg <info@dreamteam.gg>
2017-12-04 11:22:28 +0200

    multiTransfer function was added

--- contracts/DreamTeamToken.sol
+++ contracts/DreamTeamToken.sol
@@ -8,4 +8,16 @@ contract DreamTeamToken is MintableToken {
     string public symbol = "DTT";
     uint256 public decimals = 8;
 
+    function multiTransfer(address[] recipients, uint256[] amounts) public onlyOwner {
+        require(recipients.length == amounts.length);
+        uint256 sum = 0;
+        for (uint i = 0; i < amounts.length; i++) {
+            require(recipients[i] != address(0));
+            sum += amounts[i];
+        }
+        require(sum <= balances[msg.sender]);
+        for (i = 0; i < recipients.length; i++) {
+            transfer(recipients[i], amounts[i]);
+        }
+    }
 }
